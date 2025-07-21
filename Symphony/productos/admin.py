c2ad0df74a2160677bb18a7368f79fc693157c4e
KyamiV <camivalencia090@gmail.com>
2025-07-20 21:41:41 -0500

    Personalización del admin con Gestión de personas

--- Symphony/productos/admin.py
+++ Symphony/productos/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from productos.models import Producto
 
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'precio', 'cantidad_stock', 'fecha_creacion')
+    search_fields = ('nombre',)
+
+admin.site.register(Producto)
 # Register your models here.
